cea862aa23508e6892927abb6f766f0bd6310d95
tuannm99 <gm.tuannm99@gmail.com>
2023-01-17 11:43:12 +0700

    init

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -81,14 +81,7 @@ keymap("n", "<leader>dt", "<cmd>lua require'dap'.terminate()<cr>", opts)
 
 -- Lsp
 keymap("n", "<leader>lf", "<cmd>lua vim.lsp.buf.format{ async = true }<cr>", opts)
-
--- trouble
-keymap("n", "<leader>tt", "<cmd>TroubleToggle<cr>", {silent = true, noremap = true})
-keymap("n", "<leader>tw", "<cmd>TroubleToggle workspace_diagnostics<cr>", {silent = true, noremap = true})
-keymap("n", "<leader>td", "<cmd>TroubleToggle document_diagnostics<cr>", {silent = true, noremap = true})
-keymap("n", "<leader>tl", "<cmd>TroubleToggle loclist<cr>", {silent = true, noremap = true})
-keymap("n", "<leader>tq", "<cmd>TroubleToggle quickfix<cr>", {silent = true, noremap = true})
-keymap("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", {silent = true, noremap = true})
+keymap("v", "<leader>lf", "<cmd>lua vim.lsp.buf.format{ async = true }<cr>", opts)
 
 keymap('n', '<leader>e', vim.diagnostic.open_float, opts)
 keymap('n', '<leader>q', vim.diagnostic.setloclist, opts)
