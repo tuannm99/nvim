e6d3697426e610d4e68483ab0d89d91426d84297
tuannm99 <tuannm@foobla.com>
2024-07-07 23:57:29 +0700

    make cmp with snippet works

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -21,6 +21,7 @@ return {
             { "hrsh7th/cmp-path" },
             { "hrsh7th/cmp-cmdline" },
             { "hrsh7th/cmp-nvim-lua" },
+            { "saadparwaiz1/cmp_luasnip" },
         },
         config = function()
             local cmp_status_ok, cmp = pcall(require, "cmp")
@@ -36,32 +37,6 @@ return {
             local check_backspace = function()
                 local col = vim.fn.col "." - 1
                 return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
-            end
-
-            local lspkind_comparator = function(conf)
-                local lsp_types = require("cmp.types").lsp
-                return function(entry1, entry2)
-                    if entry1.source.name ~= "nvim_lsp" then
-                        if entry2.source.name == "nvim_lsp" then
-                            return false
-                        else
-                            return nil
-                        end
-                    end
-                    local kind1 = lsp_types.CompletionItemKind[entry1:get_kind()]
-                    local kind2 = lsp_types.CompletionItemKind[entry2:get_kind()]
-
-                    local priority1 = conf.kind_priority[kind1] or 0
-                    local priority2 = conf.kind_priority[kind2] or 0
-                    if priority1 == priority2 then
-                        return nil
-                    end
-                    return priority2 < priority1
-                end
-            end
-
-            local label_comparator = function(entry1, entry2)
-                return entry1.completion_item.label < entry2.completion_item.label
             end
 
             local kind_icons = {
@@ -101,41 +76,6 @@ return {
                         luasnip.lsp_expand(args.body)
                     end,
                 },
-                sorting = {
-                    -- comparators = {
-                    --     lspkind_comparator {
-                    --         kind_priority = {
-                    --             Field = 11,
-                    --             Property = 11,
-                    --             Constant = 10,
-                    --             Enum = 10,
-                    --             EnumMember = 10,
-                    --             Event = 10,
-                    --             Function = 10,
-                    --             Variable = 11,
-                    --             Method = 10,
-                    --             Operator = 10,
-                    --             Reference = 10,
-                    --             Struct = 10,
-                    --             File = 8,
-                    --             Folder = 8,
-                    --             Class = 5,
-                    --             Color = 5,
-                    --             Module = 5,
-                    --             Keyword = 2,
-                    --             Constructor = 1,
-                    --             Interface = 1,
-                    --             Snippet = 0,
-                    --             Text = 1,
-                    --             TypeParameter = 1,
-                    --             Unit = 1,
-                    --             Value = 1,
-                    --         },
-                    --     },
-                    --     label_comparator,
-                    -- },
-                },
-
                 mapping = cmp.mapping.preset.insert {
                     ["<C-k>"] = cmp.mapping.select_prev_item(),
                     ["<C-j>"] = cmp.mapping.select_next_item(),
